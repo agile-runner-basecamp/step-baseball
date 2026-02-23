7b6c570a81bde4cda4e9043f3ff1ccbb1863fa2b
이주형 <dlwngud629@naver.com>
2026-02-23 13:46:54 +0900

    ECS-28 feat: step8 - 한 턴 판정 결과 최소 구현

--- part1-tdd-practice/java/src/main/java/baseball/Game.java
+++ part1-tdd-practice/java/src/main/java/baseball/Game.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import java.util.List;
+
 public class Game {
     public boolean isEnd(int strike) {
         return strike == 3;
+    }
+
+    public String playTurn(List<Integer> answer, List<Integer> guess) {
+        Judge judge = new Judge();
+        int strike = judge.countStrike(answer, guess);
+        int ball = judge.countBall(answer, guess);
+        return judge.formatResult(ball, strike);
     }
 }
