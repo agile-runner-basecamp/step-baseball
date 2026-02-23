81ecd03b0525f36f6852d52bbcf6d75b83365ba8
이주형 <dlwngud629@naver.com>
2026-02-23 13:38:40 +0900

    ECS-28 docs: step5 - 입력 문자 검증 로직 주석 추가

--- part1-tdd-practice/java/src/main/java/baseball/InputValidator.java
+++ part1-tdd-practice/java/src/main/java/baseball/InputValidator.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class InputValidator {
     public void validate(String input) {
+        // 반환값이 void인 경우는 테스트를 우예짜는지..
         if (input.length() != 3) {
             throw new IllegalArgumentException("입력은 3자리여야 합니다.");
         }
