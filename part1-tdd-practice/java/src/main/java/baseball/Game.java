63bbbd3b3d797a78772ca070a5d512b4f86c7ae7
이주형 <dlwngud629@naver.com>
2026-02-23 13:54:40 +0900

    ECS-28 feat: step10 - 한 턴 결과와 종료 여부 반환 기능 추가

--- part1-tdd-practice/java/src/main/java/baseball/Game.java
+++ part1-tdd-practice/java/src/main/java/baseball/Game.java
@@ -15,10 +15,19 @@ public class Game {
     }
 
     public String playTurn(List<Integer> answer, String input) {
+        return playTurnWithStatus(answer, input).result();
+    }
+
+    public TurnResult playTurnWithStatus(List<Integer> answer, String input) {
+        //하나의 함수에 너무 많은 함수가 연결되는데 괜찮은건지
         InputValidator inputValidator = new InputValidator();
         InputParser inputParser = new InputParser();
+        Judge judge = new Judge();
         inputValidator.validate(input);
         List<Integer> guess = inputParser.parse(input);
-        return playTurn(answer, guess);
+        int strike = judge.countStrike(answer, guess);
+        int ball = judge.countBall(answer, guess);
+        String result = judge.formatResult(ball, strike);
+        return new TurnResult(result, isEnd(strike));
     }
 }
---
+++ part1-tdd-practice/java/src/main/java/baseball/TurnResult.java
@@ -0,0 +1,4 @@
+package baseball;
+
+public record TurnResult(String result, boolean end) {
+}
