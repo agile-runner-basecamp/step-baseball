25bdc300f40a813b63a1997f01cb6a07917b0a12
kimeunseo <kkkks9203@naver.com>
2026-02-24 03:08:08 +0900

    [ECS-27] feat: PART2-refacting-gym - step1, step2 리팩토링

--- part2-refactoring-gym/ts/step1-naming/badUmpire.test.ts
+++ part2-refactoring-gym/ts/step1-naming/badUmpire.test.ts
@@ -1,27 +1,27 @@
 import { BadUmpire } from './badUmpire';
 
 describe('BadUmpire', () => {
-    it('t1', () => {
-        const u = new BadUmpire();
-        const a = [1, 2, 3];
-        const b = [1, 4, 5];
-        const r = u.doS(a, b);
-        expect(r).toBe(1);
+    it('같은 자리에 같은 수가 있으면 스트라이크이다.', () => {
+        const umpire = new BadUmpire();
+        const target = [1, 2, 3];
+        const answer = [1, 4, 5];
+        const result = umpire.countStrike(target, answer);
+        expect(result).toBe(1);
     });
 
-    it('t2', () => {
-        const u = new BadUmpire();
-        const a = [1, 2, 3];
-        const b = [1, 2, 3];
-        const r = u.doS(a, b);
-        expect(r).toBe(3);
+    it('세 자리 모두 같으면 3스트라이크이고, 게임이 종료된다.', () => {
+        const umpire = new BadUmpire();
+        const target = [1, 2, 3];
+        const answer = [1, 2, 3];
+        const result = umpire.countStrike(target, answer);
+        expect(result).toBe(3);
     });
 
-    it('t3', () => {
-        const u = new BadUmpire();
-        const a = [1, 2, 3];
-        const b = [3, 1, 2];
-        const r = u.doB(a, b);
-        expect(r).toBe(3);
+    it('다른 자리에 같은 수가 있으면 볼이다.', () => {
+        const umpire = new BadUmpire();
+        const target = [1, 2, 3];
+        const answer = [3, 1, 2];
+        const result = umpire.countBall(target, answer);
+        expect(result).toBe(3);
     });
 });
--- part2-refactoring-gym/ts/step2-early-return/deepUmpire.ts
+++ part2-refactoring-gym/ts/step2-early-return/deepUmpire.ts
@@ -4,20 +4,18 @@ export class DeepUmpire {
     public countStrike(answer: number[] | null, guess: number[] | null): number {
         let strikeCount = 0;
 
-        if (answer !== null && guess !== null) {
-            if (answer.length === this.TARGET_SIZE && guess.length === this.TARGET_SIZE) {
-                for (let index = 0; index < this.TARGET_SIZE; index++) {
-                    if (answer[index] === guess[index]) {
-                        strikeCount++;
-                    } else {
-                        // TODO: 이 else 블록은 어떤 가치를 제공하고 있나요?
-                    }
-                }
-            } else {
-                throw new Error("입력값의 길이가 올바르지 않습니다.");
-            }
-        } else {
+        if (answer === null || guess === null) {
             throw new Error("입력값은 null일 수 없습니다.");
+        }
+
+        if(answer.length !== this.TARGET_SIZE || guess.length !== this.TARGET_SIZE){
+            throw new Error("입력값의 길이가 올바르지 않습니다.");
+        }
+
+        for (let index = 0; index < this.TARGET_SIZE; index++) {
+            if (answer[index] === guess[index]) {
+                strikeCount++;
+            }
         }
 
         return strikeCount;
