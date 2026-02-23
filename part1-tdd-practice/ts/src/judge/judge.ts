83e73cedf0189c670bf50b8c67f01ce62cf59fa4
이화목 <ghkahr1890@gmail.com>
2026-02-23 10:29:03 +0900

    [ECS-32] feat: step2

--- part1-tdd-practice/ts/src/judge/judge.ts
+++ part1-tdd-practice/ts/src/judge/judge.ts
@@ -1,17 +1,19 @@
 export interface JudgeResult {
   strikes: number;
-
+  balls: number;
 }
 
 export function judge(answer: number[], guess: number[]): JudgeResult {
   let strikes = 0;
-
+  let balls = 0;
 
   for (let i = 0; i < 3; i++) {
     if (answer[i] === guess[i]) {
       strikes++;
-    } 
+    } else if (answer.includes(guess[i])) {
+      balls++;
+    }
   }
 
-  return { strikes};
+  return { strikes, balls };
 }
