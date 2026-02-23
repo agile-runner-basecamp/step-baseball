0c767712ff8da6f8e91125327efd2495f4e2c5d1
이주형 <dlwngud629@naver.com>
2026-02-23 13:37:52 +0900

    ECS-28 feat: step5 - 입력 문자 검증 로직 추가

--- part1-tdd-practice/java/src/main/java/baseball/InputValidator.java
+++ part1-tdd-practice/java/src/main/java/baseball/InputValidator.java
@@ -5,5 +5,8 @@ public class InputValidator {
         if (input.length() != 3) {
             throw new IllegalArgumentException("입력은 3자리여야 합니다.");
         }
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("입력은 숫자만 가능합니다.");
+        }
     }
 }
